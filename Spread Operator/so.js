75a867991a1ddc9bf08859df543fe97e1c1b3c68
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-19 09:48:50 +0600

    Spread Operator with function Parameters

--- Spread Operator/so.js
+++ Spread Operator/so.js
@@ -25,3 +25,25 @@ let person = {
 let newPerson = {...person, dob: "1-1-998"}
 
 console.log(newPerson);
+
+//Spread Operator ... in Function Perameter
+
+let numbers = [10,12,585,1]
+
+console.log(Math.max(numbers)) //we want to find the max but math.max can not work with array
+
+//hence we can use spread here
+
+console.log(Math.max(...numbers)) 
+
+let person2 = ["Ferdaus", "Polok"]
+
+let test = (fanme, lname) => {
+    console.log(`Hello ${fanme} ${lname}`);
+}
+
+test(person2[0], person2[1]);
+
+//Simplifying the same procedure using spread
+
+test(...person2);
